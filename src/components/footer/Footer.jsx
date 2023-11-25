d5cd78435a520ab356e5ce01e30b4b2ba3626a5f
EMIS <you@example.com>
2023-11-25 20:32:28 +0400

    first commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './footer.module.css';
+import Image from 'next/image';
+
+function Footer() {
+  return (
+    <div className={styles.container}>
+      <div>all rights reserved</div>
+      <div className={styles.socialNewtworks}>
+      <Image src="/1.png" width={15} className={styles.icon} height={15} alt='facebook'/>
+      <Image src="/2.png" width={15} className={styles.icon} height={15} alt='instagram'/>
+      <Image src="/3.png" width={15} className={styles.icon} height={15} alt='twiter'/>
+      <Image src="/4.png" width={15} className={styles.icon} height={15} alt='youtube'/>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
