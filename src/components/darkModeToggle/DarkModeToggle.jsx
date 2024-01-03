95c4757d9dda2e3fd740078f2deefef2bce4a5b7
Tek1a <124357604+Tek1a@users.noreply.github.com>
2024-01-03 22:36:34 +0400

    Adding Single blog page and server side fetching

--- src/components/darkModeToggle/DarkModeToggle.jsx
+++ src/components/darkModeToggle/DarkModeToggle.jsx
@@ -1,25 +1,19 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import styles from "./darkModeToggle.module.css";
+import { ThemeContext } from "@/context/ThemeContext";
 
 function DarkModeToggle() {
-  const [light, setLight] = useState("left");
-
+  const { toggle, mode } = useContext(ThemeContext);
   return (
-    <div
-      className={styles.container}
-      onClick={() => {
-        if (light === "left"){
-        setLight("right");
-      }
-      else {
-        setLight("left")
-      }}}>
-
+    <div className={styles.container} onClick={toggle}>
       <div className={styles.icon}>🌙</div>
       <div className={styles.icon}>🔆</div>
-      <div className={styles.ball }  style={light === "left" ? {right: "2px"} : {left: "2px"}}/>
+      <div
+        className={styles.ball}
+        style={mode === "dark" ? { left: "2px" } : { right: "2px" }}
+      />
     </div>
   );
 }
